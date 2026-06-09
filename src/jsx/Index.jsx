3f8579c60ad27aa105f51bef4a5abcf53338aec1
Teemo Tebest <teemo.tebest@gmail.com>
2026-06-09 23:19:05 +0200

    Updated

--- src/jsx/Index.jsx
+++ src/jsx/Index.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import UNCTADSitePreview from './components/general/UNCTADSitePreview.jsx';
 
-const isLocalhost = window.location.hostname === 'localhost';
+const isLocalhost = window.location.hostname !== 'unctad.org';
 
 const container = document.getElementById(`app-root-${__PROJECT_NAME__}`);
 const root = createRoot(container);
