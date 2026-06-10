20d1ebd05342c713d9020bcd16f6501b4f1bf6e5
Teemo Tebest <teemo.tebest@gmail.com>
2026-06-10 11:35:19 +0200

    Updated conditions to show header

--- src/jsx/Index.jsx
+++ src/jsx/Index.jsx
@@ -4,7 +4,7 @@ import meta from './../meta.json';
 import App from './App.jsx';
 import UNCTADSiteHeader from './components/general/UNCTADSiteHeader.jsx';
 
-const showSiteHeader = meta.show_site_header && window.location.hostname !== 'unctad.org';
+const showSiteHeader = meta.show_site_header && !window.location.hostname.includes('unctad.org');
 
 const container = document.getElementById(`app-root-${__PROJECT_NAME__}`);
 const root = createRoot(container);
