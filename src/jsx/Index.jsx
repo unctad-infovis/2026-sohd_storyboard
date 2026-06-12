aa67ce274f8455f9bc42cb8d8af0b3881aaf59d9
Teemo Tebest <teemo.tebest@gmail.com>
2026-06-12 17:00:36 +0200

    Added parameter to hide hader

--- src/jsx/Index.jsx
+++ src/jsx/Index.jsx
@@ -4,7 +4,9 @@ import meta from './../meta.json';
 import App from './App.jsx';
 import UNCTADSiteHeader from './components/general/UNCTADSiteHeader.jsx';
 
-const showSiteHeader = meta.show_site_header && !window.location.hostname.includes('unctad.org');
+const params = new URLSearchParams(window.location.search);
+const headerParam = params.get('header');
+const showSiteHeader = headerParam !== null ? headerParam !== 'false' && headerParam !== '0' : meta.show_site_header && !window.location.hostname.includes('unctad.org');
 
 const container = document.getElementById(`app-root-${__PROJECT_NAME__}`);
 const root = createRoot(container);
